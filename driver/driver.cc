00a0659ffa67f1e14a68c33664754251f8358dc9
Benjamin <benjamin.kisliuk@dfki.de>
2018-05-04 10:06:27 +0000

    added additional files

---
+++ driver/driver.cc
@@ -0,0 +1,15 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "driver.h"
+
+void driver_startup()
+{
+    /* Write your initialization code here,
+       but do not make any call to a required interface. */
+}
+
+void driver_PI_clock()
+{
+    /* Write your code here! */
+}
+
---
+++ driver/driver.h
@@ -0,0 +1,23 @@
+/* This file was generated automatically: DO NOT MODIFY IT ! */
+
+/* Declaration of the functions that have to be provided by the user */
+
+#ifndef __USER_CODE_H_driver__
+#define __USER_CODE_H_driver__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void driver_startup();
+
+void driver_PI_clock();
+
+extern void driver_RI_trigger_out();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
